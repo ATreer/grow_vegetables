290fefb83cf2df267ef6d51451a6d7df27f0c917
huangzhen <1099703970@qq.com>
2022-01-14 11:13:55 +0800

    添加data 注解

--- grow_vegetable_service/src/main/java/com/hz/vegetable/domain/status/SelfStatus.java
+++ grow_vegetable_service/src/main/java/com/hz/vegetable/domain/status/SelfStatus.java
@@ -1,10 +1,13 @@
 package com.hz.vegetable.domain.status;
 
+import lombok.Data;
+
 /**
  * @author HuangZ
  * @version v1.0
  * @date 2022/1/14
  **/
+@Data
 public class SelfStatus {
     int type;
     String name;

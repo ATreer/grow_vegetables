975a8b087781d3c5fd9ccc460fa9868c54ec28b5
ATreer <1433323626@qq.com>
2022-01-16 21:27:07 +0800

    2022年1月16日21:27:01

---
+++ portal/src/components/Welcome.vue
@@ -0,0 +1,19 @@
+<template>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-main>Main</el-main>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

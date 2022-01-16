643dd039ca69a19682deaed272339c2b75e7ef54
ATreer <1433323626@qq.com>
2022-01-16 22:45:52 +0800

    2022年1月16日22:45:45

--- portal/src/main.js
+++ portal/src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import router from "./router/router.js"
+import resource from 'vue-resource';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
-
+Vue.use(ElementUI)
+Vue.use(resource)
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
--- portal/src/router/router.js
+++ portal/src/router/router.js
@@ -7,18 +7,14 @@ import main from "../components/MainView";
 // 要告诉 vue 使用 vueRouter
 Vue.use(VueRouter);
 
-const routes = [
-    {
-        path:"/",
-        component: main
-    },
-    {
-        path: "/about",
-        component: about
-    }
-]
-
-var router =  new VueRouter({
-    routes
-})
-export default router;
+export default new VueRouter({
+    routes: [
+        {
+            path:"/",
+            component: main
+        },{
+            path:"/main",
+            component: main
+        }
+    ]
+});
